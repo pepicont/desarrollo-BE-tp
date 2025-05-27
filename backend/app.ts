bf1016309df6612169d3df266acdedfb8003df69
pepicont <stefano.conti2004@gmail.com>
2025-05-27 17:55:22 -0300

    decomment linea fallada (req erronea), agregue test de esto en requests

--- backend/app.ts
+++ backend/app.ts
@@ -3,11 +3,12 @@ import { companiaRouter } from './src/Compania/compania.routes.js'
 const app = express()
 app.use(express.json())
 
-app.use('/api/compania', companiaRouter) //companiaRouter)
+app.use('/api/compania', companiaRouter)
 
-/*app.use((_, res) => {
-  return res.status(404).send({ message: 'Resource not found' })
-})*/
+app.use((_, res) => {
+  res.status(404).send({ message: 'Resource not found' })
+  return
+})
 
 app.listen(3000, () => {
   console.log('Server runnning on http://localhost:3000/')
