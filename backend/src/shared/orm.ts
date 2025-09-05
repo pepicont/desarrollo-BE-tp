e2ed337dfbe3f8692a5bbef935467a398b9b450e
Augusto <brizioaugusto@gmail.com>
2025-09-05 16:54:37 -0300

    Docker compose

--- backend/src/shared/orm.ts
+++ backend/src/shared/orm.ts
@@ -9,6 +9,12 @@ import { Usuario } from '../Usuario/usuario.entity.js';
 import {Venta} from '../Venta/venta.entity.js'
 import { Resenia } from '../Resenia/resenia.entity.js';
 
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_PORT = Number(process.env.DB_PORT || 3307)
+const DB_USER = process.env.DB_USER || 'dsw'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'dsw' 
+const DB_NAME = process.env.DB_NAME || 'portalvideojuegos'
+
 export const orm = await MikroORM.init({
   //entities: ['dist/**/*.entity.js'],
   //entitiesTs: ['src/**/*.entity.ts'],
@@ -18,9 +24,9 @@ export const orm = await MikroORM.init({
   entitiesTs: [
     Categoria, Compania, Servicio, Juego, Complemento, Usuario, Venta, Resenia
   ],
-  dbName: 'portalvideojuegos',
+  dbName: DB_NAME,
   driver: MySqlDriver,
-  clientUrl: 'mysql://dsw:dsw@localhost:3307/portalvideojuegos',
+  clientUrl: `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   debug: true,
   schemaGenerator: {
     //never in production
