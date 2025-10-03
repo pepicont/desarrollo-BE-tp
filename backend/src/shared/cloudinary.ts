71838f8c34256ca186b1133920614e4ed5bcc13b
pepicont <stefano.conti2004@gmail.com>
2025-10-03 10:24:06 -0300

    prueba cloudinary deploy

--- backend/src/shared/cloudinary.ts
+++ backend/src/shared/cloudinary.ts
@@ -1,9 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+if (process.env.NODE_ENV === 'production') {
+  cloudinary.config({ cloudinary_url: process.env.CLOUDINARY_URL });
+} else {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+}
 
 export { cloudinary };
